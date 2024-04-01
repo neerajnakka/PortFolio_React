2035b1bf925c31e4539dd5c7bcff96252bf0fa28
neerajnakka <neerajnakka8@gmail.com>
2024-04-01 16:38:10 +0530

    D

--- components/NotFound.jsx
+++ components/NotFound.jsx
@@ -3,7 +3,7 @@ function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
-      <p className="text-lg mb-8">The page you'r looking for doesn't exist.</p>
+      <p className="text-lg mb-8">The page does not exist.</p>
       <Link
         to="/"
         className="text-lg font-semibold text-purple-500 hover:text-purple-700 transition duration-300"
