6a097282ba270bf4f1f49dade4ba7d54c6b8130f
neerajnakka <65122791+neerajnakka@users.noreply.github.com>
2024-04-11 19:33:46 +0530

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -9,31 +9,10 @@ export const ABOUT_TEXT = `I am Neeraj Chandra Nakka, a recent Computer Science 
 // export const EXPERIENCES = [
 //   {
 //     year: '2023 - Present',
-//     role: 'Senior Full Stack Developer',
-//     company: 'Google Inc.',
-//     description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+//     role: ' Full Stack Developer',
+//     company: '',
+//     description: ``,
 //     technologies: ['Javascript', 'React.js', 'Next.js', 'mongoDB'],
-//   },
-//   {
-//     year: '2022 - 2023',
-//     role: 'Frontend Developer',
-//     company: 'Adobe',
-//     description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-//     technologies: ['HTML', 'CSS', 'Vue.js', 'mySQL'],
-//   },
-//   {
-//     year: '2021 - 2022',
-//     role: 'Full Stack Developer',
-//     company: 'Facebook',
-//     description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-//     technologies: ['Python', 'Svelte', 'Three.js', 'Postgres'],
-//   },
-//   {
-//     year: '2020 - 2021',
-//     role: 'Software Engineer',
-//     company: 'Paypal',
-//     description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-//     technologies: ['Ruby', 'Rails', 'PHP', 'Sqlite'],
 //   },
 // ];
 
